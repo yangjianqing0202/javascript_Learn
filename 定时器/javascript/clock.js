134bbd32289051936490fa04892082063e5eed2c
yangjianqing0202 <263522637@qq.com>
2017-03-29 00:22:24 +0800

    2

--- 定时器/javascript/clock.js
+++ 定时器/javascript/clock.js
@@ -1,3 +1,36 @@
+
+
+
+window.onload = function()
+{
+    var oDiv1 = document.getElementById('time');
+    var aImg1 = oDiv1.getElementsByTagName('img');
+    var oImg2 = document.getElementsByTagName('week');
+        function tick()
+        {
+            var oDate = new Date;
+            var str1 = toDou(oDate.getHours()) + toDou(oDate.getMinutes()) + toDou(oDate.getSeconds());
+
+            var iWeek=(oDate.getDay()+6)%7;//星期
+            var week_name= ["one", "two", "three", "four", "five", "six", "seven"];
+
+            for (var i=0;i<aImg1.length;i++)
+            {
+                aImg1[i].src = 'images/'+str1.charAt(i)+'.png';
+            }
+            for (var i=0;i<oImg2.length;i++)
+            {
+                oImg2[i].src='images/'+week_name[iWeek]+'.png';
+            }
+
+
+        }
+        setInterval(tick,1000);
+        tick();
+
+};
+
+
 function toDou (n)
 {
     if(n<10)
@@ -9,23 +42,3 @@ function toDou (n)
         return '' + n
     }
 }
-
-window.onload = function()
-{
-    var oDiv1 = document.getElementById('time');
-    var aImg1 = oDiv1.getElementsByTagName('img');
-
-        function tick()
-        {
-            var oDate = new Date;
-            var str = toDou(oDate.getHours()) + toDou(oDate.getMinutes()) + toDou(oDate.getSeconds());
-
-            for (var i=0;i<aImg1.length;i++)
-            {
-                aImg1[i].src = 'images/'+str[i]+'.png';
-            }
-        }
-        setInterval(tick,1000);
-        tick();
-
-};
